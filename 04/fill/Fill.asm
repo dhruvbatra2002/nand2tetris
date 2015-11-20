40bccf9173e62a85316c64086de345452ba049ab
OJ Haugen <ohaugen@usc.edu>
2015-11-19 22:56:36 -0800

    Project 4 CompleteProject 4 CompleteProject 4 CompleteProject 4 Complete

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,77 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+// ---------------------------------
+
+
+// Store screen address
+@SCREEN
+D = A
+@addr
+M = D
+
+// Store keyboard address
+@KBD
+D = A
+@addrK
+M = D
+
+
+(LOOP)
+	@KBD // checking current state of KBD
+	D=M;
+	@BLACKEN // if state != 0, blacken screen
+	D;JNE
+	// else:
+	@WHITEN  // otherwise (if state = 0), whiten screen
+	0;JMP
+
+(WHITEN)
+	// reset current spot to first spot in SCREEN memory map
+	@addr 
+	D = M
+	@curr
+	M = D
+			
+	@WHITEN_LOOP // loop through every address in SCREEN memory map
+	0; JMP
+
+(WHITEN_LOOP)
+	@addrK // calc distance of current spot to KBD
+	D = M - D
+	@LOOP // if distane is 0, stop and go back to main LOOP
+	D; JEQ
+	
+	@curr
+	AD = M
+	M = 0
+	@curr
+	MD = D + 1
+	@WHITEN_LOOP
+	0; JMP
+
+(BLACKEN)
+	@addr
+	D = M
+	@curr
+	M = D
+	@BLACKEN_LOOP
+	0; JMP
+
+(BLACKEN_LOOP)
+	@addrK
+	D = M - D
+	@LOOP
+	D; JEQ
+	@curr
+	AD = M
+	M = -1
+	@curr
+	MD = D + 1
+	@BLACKEN_LOOP
+	0; JMP
+
+
+
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// initialize variables
+@R2
+M = 0
+@count
+M = 0
+
+
+// add R0 to R2 R1 times:
+(LOOP)	
+	// if count = r1, end program
+	@R1
+	D = M
+	@count
+	D = M - D
+	@END
+	D; JEQ
+	
+	// add R0 to R2
+	@R0
+	D = M
+	@R2
+	M = M + D
+	
+	// iterate count
+	@count
+	M = M + 1
+	
+	// do another loop
+	@LOOP
+	0; JMP
+	
+
+(END)
+	@END
+	0; JMP
+
